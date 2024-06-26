630814eb17723595c92e2c8bac3053e5b8499f9e
Ramraj <155386331+Shriram-S-Bhople@users.noreply.github.com>
2024-06-26 10:23:13 +0530

    first commit

---
+++ 01hello/01Hello.js
@@ -0,0 +1,3 @@
+console.log("hello world its ram learning the js ");
+// to run the go to the file path hello world
+// PS C:\Users\shrir\OneDrive\Desktop\javascript> node filename.js;
---
+++ 02basic/01Varable.js
@@ -0,0 +1,7 @@
+// var ,let const
+var full_name="Shriram Sakhahari Bhople";
+var courseName="JavaScript Course";
+var isBoolean=true;// True ans true is different
+var loggedCount=10;
+console.log(loggedCount);
+//  so basically there are also more data types we have seen 
